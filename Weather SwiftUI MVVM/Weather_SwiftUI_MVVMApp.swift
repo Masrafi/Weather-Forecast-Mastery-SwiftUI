04f2c99469f8c16eefb5b1a0315116b6df736576
Masrafi <masrafianam@gmail.com>
2024-08-09 15:01:36 +0600

    Initial Commit

---
+++ Weather SwiftUI MVVM/Weather_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUI_MVVMApp.swift
+//  Weather SwiftUI MVVM
+//
+//  Created by Md Khorshed Alam on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
